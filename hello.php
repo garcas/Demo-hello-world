70bfe41dbe62e5e824aad37c904c9dfde4e6c4a9
Pablo <example@example.com>
2013-11-28 15:51:58 +0100

    Ajouter javascript

---
+++ hello.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Demo Hello World</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    </head>
+    <body>
+        <div><h1>C'est le deuxième version du Demo hello world</h1></div>
+		<a href=""  >mettre à jour</a><br/>
+		<a href="#" onClick="javascript:alert('Bonjour')">saluer</a>
+    </body>
+
+</html>
