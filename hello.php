d4420bf318c625423ca8e2dc963244147149bea9
Pablo <example@example.com>
2013-11-28 16:41:13 +0100

    bonsoir

--- hello.php
+++ hello.php
@@ -8,6 +8,7 @@
         <div><h1>C'est le deuxième version du Demo hello world</h1></div>
 		<a href=""  >mettre à jour</a><br/>
 		<a href="#" onClick="javascript:alert('Bonjour')">saluer</a>
+		<a href="#" onClick="javascript:alert('bonsoir')">bonsoir</a>
     </body>
 
 </html>
